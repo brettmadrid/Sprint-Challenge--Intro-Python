3da0dc351f8ac1d353dda336ad592108fc7502b2
brettmadrid <brettmadrid9@gmail.com>
2019-08-18 18:14:37 -0700

    Completed oop2.  Tests pass

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
@@ -36,3 +36,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for i in vehicles:
+    print(i.drive())
